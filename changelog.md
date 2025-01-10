031b39b19b7d3496603a68055937dea1865de037
tjf1 <fajnytomek9@gmail.com>
2025-01-10 21:28:27 +0100

    v0.0.2 - basic file stuff and a status button

--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
+# v0.0.2
+- Added a button in the pause menu that shows current save status, and broadcasting progress to file.
+
 # v0.0.1
 - First version! No stuff yet, just basic setup code.
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,67 @@
 #include <Geode/Geode.hpp>
-FLAlertLayer* info_alert = nullptr;
-using namespace geode::prelude;
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/binding/PlayerObject.hpp>
+#include <Geode/binding/GJGameLevel.hpp> 
+#include <fstream>
+#include <sstream>
+#include <nlohmann/json.hpp>  
+using json = nlohmann::json;  
+
+using namespace geode::prelude;
+
 class $modify(ModPauseLayer, PauseLayer) {
+public:
+    
+
+
+    void saveLevelProgress(float dt) {
+        // data
+        auto level = PlayLayer::get()->m_level;
+        auto levelName = level->m_levelName;
+        auto levelID = level->m_levelID;
+        auto player1 = PlayLayer::get()->m_player1;
+        auto player2 = PlayLayer::get()->m_player2;
+        auto player1PosX = player1->m_positionX;
+        auto player1PosY = player1->m_positionY;
+        auto player2PosX = player2->m_positionX;
+        auto player2PosY = player2->m_positionY;
+        auto now = std::chrono::system_clock::now();
+        auto timestamp = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count();
+        json current;
+        current["level"] = { {"name", levelName}, {"id", std::to_string(levelID)} };
+        current["player"] = {
+            {"player1", {{"x", std::to_string(player1PosX)}, {"y", std::to_string(player1PosY)}}},
+            {"player2", {{"x", std::to_string(player2PosX)}, {"y", std::to_string(player2PosY)}}}
+        };
+        current["timestamp"] = std::to_string(timestamp);
+
+        // load
+        std::ifstream inFile("level_progress.json");
+        json allProgress;
+        if (inFile.is_open()) {
+            inFile >> allProgress;
+            inFile.close();
+        }
+
+        // replace
+        allProgress["levels"][std::to_string(levelID)] = current;
+
+        // write
+        std::ofstream outFile("level_progress.json");
+        outFile << std::setw(4) << allProgress << std::endl;
+        outFile.close();
+        log::debug("level progress has saved.");
+    }
+
 
     void customSetup() {
+        float saveInterval = 5.0f; 
         PauseLayer::customSetup();
 
+        
         auto winSize = CCDirector::get()->getWinSize();
         auto level = PlayLayer::get()->m_level;
-
+            
         auto menu = this->getChildByID("left-button-menu");
 
         auto btn_spr = CCSprite::createWithSpriteFrameName("GJ_restartCheckBtn_001.png");
@@ -24,37 +75,34 @@ class $modify(ModPauseLayer, PauseLayer) {
         btn->setPosition({ menu->getContentSize().width / 2, btn->getContentSize().height / 2 });
         menu->addChild(btn);
         menu->updateLayout();
+        this->schedule(schedule_selector(ModPauseLayer::saveLevelProgress), 5, kCCRepeatForever, 0);
+    }
 
-    }
+    
     void onCheckpoint(CCObject * sender) {
-        FLAlertLayer::create(
-            "Checkpoint",    // title
-            "Your progress is being <cg>saved</c> live.",  // content
-            "OK"        // button
-        )->show();
+        auto level = PlayLayer::get()->m_level;
+        auto levelName = level->m_levelName;
+        auto levelID = level->m_levelID;
+        auto isPlatformer = PlayLayer::get()->m_isPlatformer;
+        log::info("Platformer? {}, in {}", isPlatformer, levelName);
+
+        if (isPlatformer) {
+            FLAlertLayer::create(
+                "Checkpoint",
+                "Progress in this level is <cg>being saved</c>.",
+                "OK"
+            )->show();
+        }
+        else {
+            FLAlertLayer::create(
+                "Checkpoint",
+                "Progress in this level is <cr>not being saved</c>.",
+                "OK"
+            )->show();
+        }
     }
+
     void onResume(CCObject * sender) {
         PauseLayer::onResume(sender);
     }
-};
-#include <Geode/modify/PlayLayer.hpp>
-
-class $modify(ModPlayLayer, PlayLayer) {
-public:
-    bool init() {
-
-        return true;
-    }
-
-    void printPlayerPosition(PlayerObject& player) {
-        float posX = player.getPosition().x;
-        float posY = player.getPosition().y;
-
-        log::info(fmt::format("Player Position: x = {} y = {}", posX, posY));
-    }
-
-
-
-
-
 };
